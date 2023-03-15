a9983bb360129d19fe102f57ad0ddfe3689e916d
Kieran <kieran.webdevelopment@gmail.com>
2023-03-15 17:24:32 +0000

    handlechange and rudimentary handlesubmit functions created

--- src/Components/QuizPage.jsx
+++ src/Components/QuizPage.jsx
@@ -1,7 +1,10 @@
 import React, { useState } from 'react';
 
 export default function QuizPage({ quizData }) {
-  const [formData, setFormData] = useState(defaultform);
+  const [usersAnswers, setusersAnswers] = useState({});
+
+  const correctAnswers = quizData.map((item) => item.correct_answer);
+  console.log(correctAnswers);
 
   function defaultform() {
     const defaultForm = {};
@@ -11,28 +14,74 @@ export default function QuizPage({ quizData }) {
     }
     return defaultForm;
   }
-  console.log(quizData);
-  console.log(formData);
 
-  function handleSubmit() {}
+  // console.log(quizData);
+  // console.log(formData);
+  function handleChange(event) {
+    const { name, value, checked } = event.target;
+    setusersAnswers((prevValue) => {
+      return {
+        ...prevValue,
+        [name]: value,
+      };
+    });
+    console.log(usersAnswers);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(usersAnswers);
+    // setScore();
+  }
+
+  function convertToQuotations(string) {
+    return decodeURIComponent(
+      string.replace(/&quot;/g, '"').replace(/&#039;/g, "'")
+    );
+  }
 
   const questions = quizData.map((item, index) => {
-    console.log(item.allOptionsShuffled);
+    // console.log(item.allOptionsShuffled);
     const option = item.allOptionsShuffled;
+
     return (
-      <div>
-        <h1>{decodeURIComponent(item.question.replace(/&quot;/g, '"'))}</h1>
-        <input type="radio" id={option[0]} name={index + 1} value={option[0]} />
-        <label htmlFor={option[0]}>{option[0]}</label>
+      <div key={item.question}>
+        <h1>{convertToQuotations(item.question)}</h1>
+        <input
+          type="radio"
+          id={option[0]}
+          name={`question${index + 1}`}
+          value={option[0]}
+          onChange={handleChange}
+        />
+        <label htmlFor={option[0]}>{convertToQuotations(option[0])}</label>
 
-        <input type="radio" id={option[1]} name={index + 1} value={option[1]} />
-        <label htmlFor={option[1]}>{option[1]}</label>
+        <input
+          type="radio"
+          id={option[1]}
+          name={`question${index + 1}`}
+          value={option[1]}
+          onChange={handleChange}
+        />
+        <label htmlFor={option[1]}>{convertToQuotations(option[1])}</label>
 
-        <input type="radio" id={option[2]} name={index + 1} value={option[2]} />
-        <label htmlFor={option[2]}>{option[2]}</label>
+        <input
+          type="radio"
+          id={option[2]}
+          name={`question${index + 1}`}
+          value={option[2]}
+          onChange={handleChange}
+        />
+        <label htmlFor={option[2]}>{convertToQuotations(option[2])}</label>
 
-        <input type="radio" id={option[3]} name={index + 1} value={option[3]} />
-        <label htmlFor={option[3]}>{option[3]}</label>
+        <input
+          type="radio"
+          id={option[3]}
+          name={`question${index + 1}`}
+          value={option[3]}
+          onChange={handleChange}
+        />
+        <label htmlFor={option[3]}>{convertToQuotations(option[3])}</label>
       </div>
     );
   });
