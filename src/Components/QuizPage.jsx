6cfa69e449abea83efe60cda2c0d952dc55b86f9
Kieran <kieran.webdevelopment@gmail.com>
2023-03-15 16:02:00 +0000

    added function to set default formdata object based on the amount of questions in the quiz

--- src/Components/QuizPage.jsx
+++ src/Components/QuizPage.jsx
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function QuizPage({ quizData }) {
+  const [formData, setFormData] = useState(defaultform);
+
+  function defaultform() {
+    const defaultForm = {};
+
+    for (let i = 0; i < quizData.length; i++) {
+      defaultForm[`question${i + 1}`] = '';
+    }
+    return defaultForm;
+  }
   console.log(quizData);
+  console.log(formData);
+
+  function handleSubmit() {}
 
   const questions = quizData.map((item, index) => {
     console.log(item.allOptionsShuffled);
@@ -20,18 +33,14 @@ export default function QuizPage({ quizData }) {
 
         <input type="radio" id={option[3]} name={index + 1} value={option[3]} />
         <label htmlFor={option[3]}>{option[3]}</label>
-
-        {/* <label htmlFor="">
-          <h1>{decodeURIComponent(item.question.replace(/&quot;/g, '"'))}</h1>
-          <input
-            type="radio"
-            name={index + 1}
-            value={item.allOptionsShuffled[0]}
-          />
-        </label> */}
       </div>
     );
   });
 
-  return <form>{questions}</form>;
+  return (
+    <form onSubmit={handleSubmit}>
+      {questions}
+      <button>Check Answers</button>
+    </form>
+  );
 }
